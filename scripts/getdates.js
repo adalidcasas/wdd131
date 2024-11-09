fa2b8ba3986988adf5771a96db0b42edcbcded58
adalidcasas <cas19040@byui.edu>
2024-11-09 16:52:25 -0400

    scripts udpdate

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const this_year = document.querySelector("#currentyear");
+const last_modified = document.querySelector("#lastModified");
+
+let today = new Date();
+let last_date = new Date(document.lastModified);
+
+this_year.innerHTML = today.getFullYear();
+last_modified.innerHTML = `Last Modified:  <span class="highlight">${last_date}</span>`;
+
